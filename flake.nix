a015cb1f2b6fdeebd009e3229ee0bcc3817f27e4
darknebula05 <camms205@aol.com>
2024-03-28 14:58:26 -0400

    Add rust devShell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = fenix.packages.${system}.stable;
+      in
+      {
+        devShells.rust = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (toolchain.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
